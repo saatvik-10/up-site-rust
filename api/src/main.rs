453bf12041a1481de76a64b2ba1df8fd97b23bb1
saatvik-10 <saatvikmadan529@gmail.com>
2025-11-30 15:32:22 +0530

    added poem

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,7 +1,22 @@
-use std::fmt::Error;
+use poem::{Route, Server, get, handler, listener::TcpListener, post, web::Path};
+
+#[handler]
+fn create_website(Path(website_id): Path<String>) -> String {
+    format!("hello: {}", website_id)
+}
+
+#[handler]
+fn get_website(Path(website_id): Path<String>) -> String {
+    format!("hello: {}", website_id)
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    print!("Hello World");
-    Ok(())
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new()
+        .at("/status/:website_id", get(get_website))
+        .at("/website", post(create_website));
+
+    Server::new(TcpListener::bind("0.0.0.0:3001"))
+        .run(app)
+        .await
 }

dbca3c056dbbe3163ee6127992f6d3666d2f4682
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-02 14:31:35 +0530

    updated lib fn

---
+++ db/src/lib.rs
@@ -0,0 +1,22 @@
+pub struct Db {
+    conn: Connection
+}
+
+impl Default for Db {
+    fn default() -> Self {
+        
+        Self { 
+            conn
+        }
+    }
+}
+
+impl Db {
+    pub fn create_user(&self) {
+        println!("Create User");
+    }
+
+    pub fn create_website(&self) -> String {
+        String::from("1")
+    }
+}

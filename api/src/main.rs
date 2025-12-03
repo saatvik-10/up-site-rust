105dcf1db41290ef2509bb8a0f2e562713c80cac
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-03 19:05:58 +0530

    added user handlers

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,21 +1,24 @@
-use crate::states::{CreateWebsiteInput, CreateWebsiteOutput};
+use crate::states::{JwtOutput, UserInput, UserOutput, WebsiteInput, WebsiteOutput};
 use db::db::Db;
 use poem::{
     Route, Server, get, handler,
     listener::TcpListener,
     post,
-    web::{Json, Path},
+    web::{self, Json, Path},
 };
 
 pub mod states;
 
 #[handler]
-fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput> {
+fn create_website(Json(data): Json<WebsiteInput>) -> Json<WebsiteOutput> {
     // let url = data.url;
     let mut d = Db::default().unwrap();
     let website = d.create_website(String::from("1"), data.url).unwrap();
 
-    let res = CreateWebsiteOutput { id: website.id };
+    let res = WebsiteOutput {
+        id: website.id,
+        url: website.url,
+    };
 
     Json(res)
 }
@@ -23,6 +26,31 @@ fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOut
 #[handler]
 fn get_website(Path(website_id): Path<String>) -> String {
     format!("hello: {}", website_id)
+}
+
+#[handler]
+fn user_sign_up(Json(data): Json<UserInput>) -> Json<UserOutput> {
+    let mut d = Db::default().unwrap();
+    let user = d.user_sign_up(data.username, data.password).unwrap();
+
+    let res = UserOutput {
+        id: user.id,
+        username: user.username,
+    };
+
+    Json(res)
+}
+
+#[handler]
+fn user_sign_in(Json(data): Json<UserInput>) -> Json<JwtOutput> {
+    let mut d = Db::default().unwrap();
+    let user = d.user_sign_up(data.username, data.password).unwrap();
+
+    let res = JwtOutput {
+        jwt: String::from("sm"),
+    };
+
+    Json(res)
 }
 
 #[tokio::main]

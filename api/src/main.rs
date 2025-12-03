4d1ea15921359b61363fdad670dd10c047e637ca
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-04 00:50:13 +0530

    thread safe data

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,72 +1,24 @@
-use crate::states::{JwtOutput, UserInput, UserOutput, WebsiteInput, WebsiteOutput};
-use db::db::Db;
-use poem::{
-    Route, Server, get, handler,
-    listener::TcpListener,
-    post,
-    web::{Json, Path},
+use crate::handlers::{
+    user::{user_sign_in, user_sign_up},
+    website::{create_website, get_website},
 };
+use db::db::Db;
+use poem::{EndpointExt, Route, Server, get, listener::TcpListener, post};
+use std::sync::{Arc, Mutex};
 
+pub mod handlers;
 pub mod states;
 
-#[handler]
-fn create_website(Json(data): Json<WebsiteInput>) -> Json<WebsiteOutput> {
-    let mut d = Db::default().unwrap();
-    let website = d.create_website(String::from("1"), data.url).unwrap();
-
-    let res = WebsiteOutput {
-        id: website.id,
-        url: website.url,
-    };
-
-    Json(res)
-}
-
-#[handler]
-fn get_website(Path(website_id): Path<String>) -> Json<WebsiteOutput> {
-    let mut d = Db::default().unwrap();
-    let website = d.get_website(website_id).unwrap();
-
-    let res = WebsiteOutput {
-        id: website.id,
-        url: website.url,
-    };
-
-    Json(res)
-}
-
-#[handler]
-fn user_sign_up(Json(data): Json<UserInput>) -> Json<UserOutput> {
-    let mut d = Db::default().unwrap();
-    let user = d.user_sign_up(data.username, data.password).unwrap();
-
-    let res = UserOutput {
-        id: user.id,
-        username: user.username,
-    };
-
-    Json(res)
-}
-
-#[handler]
-fn user_sign_in(Json(data): Json<UserInput>) -> Json<JwtOutput> {
-    let mut d = Db::default().unwrap();
-    let _user = d.user_sign_in(data.username, data.password).unwrap();
-
-    let res = JwtOutput {
-        jwt: String::from("sm"),
-    };
-
-    Json(res)
-}
-
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), std::io::Error> {
+    let db = Arc::new(Mutex::new(Db::default().unwrap()));
+
     let app = Route::new()
         .at("/status/:website_id", get(get_website))
         .at("/website", post(create_website))
         .at("/user/sign-up", post(user_sign_up))
-        .at("/user/sign-in", post(user_sign_in));
+        .at("/user/sign-in", post(user_sign_in))
+        .data(db);
 
     Server::new(TcpListener::bind("0.0.0.0:3001"))
         .run(app)

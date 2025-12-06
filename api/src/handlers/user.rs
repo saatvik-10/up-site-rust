0b8b8e55c71764ab7b7c2aa1f43b3ebce03f34c9
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-06 17:32:43 +0530

    updated sign in handler

--- api/src/handlers/user.rs
+++ api/src/handlers/user.rs
@@ -3,12 +3,16 @@ use std::sync::{Arc, Mutex};
 use crate::states::{JwtOutput, UserInput, UserOutput};
 use db::db::Db;
 use poem::{
-    handler,
+    Error, handler,
+    http::StatusCode,
     web::{Data, Json},
 };
 
 #[handler]
-pub fn user_sign_up(Json(data): Json<UserInput>, Data(db): Data<&Arc<Mutex<Db>>>) -> Json<UserOutput> {
+pub fn user_sign_up(
+    Json(data): Json<UserInput>,
+    Data(db): Data<&Arc<Mutex<Db>>>,
+) -> Json<UserOutput> {
     let mut db_lock = db.lock().unwrap();
     let user = db_lock.user_sign_up(data.username, data.password).unwrap();
 
@@ -21,13 +25,18 @@ pub fn user_sign_up(Json(data): Json<UserInput>, Data(db): Data<&Arc<Mutex<Db>>>
 }
 
 #[handler]
-pub fn user_sign_in(Json(data): Json<UserInput>, Data(db): Data<&Arc<Mutex<Db>>>) -> Json<JwtOutput> {
+pub fn user_sign_in(
+    Json(data): Json<UserInput>,
+    Data(db): Data<&Arc<Mutex<Db>>>,
+) -> Result<Json<JwtOutput>, Error> {
     let mut db_lock = db.lock().unwrap();
-    let _user = db_lock.user_sign_in(data.username, data.password).unwrap();
+    let user_id = db_lock.user_sign_in(data.username, data.password);
 
-    let res = JwtOutput {
-        jwt: String::from("sm"),
-    };
-
-    Json(res)
+    match user_id {
+        Ok(user_id) => {
+            let res = JwtOutput { jwt: user_id };
+            Ok(Json(res))
+        }
+        Err(_e) => Err(Error::from_status(StatusCode::UNAUTHORIZED)),
+    }
 }
